4f3378d20d3bc89228020a4754476534c8b01c99
Ayomide Adekoya <ayomideadekoya266@gmail.com>
2025-06-20 09:15:00 -0700

    add tab navigation

---
+++ Toki/Views/ContentView.swift
@@ -0,0 +1,22 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView {
+            ChatView()
+                .tabItem {
+                    Label("Ask", systemImage: "message")
+                }
+            
+            ExploreView()
+                .tabItem {
+                    Label("Explore", systemImage: "magnifyingglass")
+                }
+            
+            ProfileView()
+                .tabItem {
+                    Label("Profile", systemImage: "person")
+                }
+        }
+    }
+}

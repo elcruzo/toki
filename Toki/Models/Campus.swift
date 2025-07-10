067ae9d406f75b65915064d8fe802f37e9d564d9
Ayomide Adekoya <ayomideadekoya266@gmail.com>
2025-07-10 09:00:00 -0700

    add campus models

---
+++ Toki/Models/Campus.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+struct Campus: Identifiable, Codable {
+    let id: String
+    let name: String
+    let shortName: String
+    let location: String
+    let studentCount: Int
+}
+
+struct DiningHall: Identifiable, Codable {
+    let id: String
+    let name: String
+    let isOpen: Bool
+    let currentWait: Int?
+    let menu: [MenuItem]
+}
+
+struct MenuItem: Identifiable, Codable {
+    let id: String
+    let name: String
+    let station: String
+}

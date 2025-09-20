d3bce291c6c2f6ec75708ea1cb6360cb7ba5e73e
Ayomide Adekoya <ayomideadekoya266@gmail.com>
2025-09-20 14:15:00 -0700

    add loading and empty states

---
+++ Toki/Views/LoadingView.swift
@@ -0,0 +1,32 @@
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        VStack(spacing: 16) {
+            ProgressView()
+                .scaleEffect(1.5)
+            Text("loading...")
+                .foregroundColor(.secondary)
+        }
+    }
+}
+
+struct EmptyStateView: View {
+    let title: String
+    let message: String
+    let icon: String
+    
+    var body: some View {
+        VStack(spacing: 16) {
+            Image(systemName: icon)
+                .font(.system(size: 48))
+                .foregroundColor(.secondary)
+            Text(title)
+                .font(.headline)
+            Text(message)
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+        }
+        .padding()
+    }
+}
